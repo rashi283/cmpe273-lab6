b5c8c21e4f7435b5e76441617ab137b750ef921e
Rashi Agrawal <rashi283@gmail.com>
2014-05-01 09:33:11 -0700

    Lab 6 submission

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,41 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
-
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
+        
+        List<CacheServiceInterface> servers = new ArrayList<CacheServiceInterface>();
+        servers.add(new DistributedCacheService("http://localhost:3000"));
+        servers.add(new DistributedCacheService("http://localhost:3001"));
+        servers.add(new DistributedCacheService("http://localhost:3002"));
+        
+        String[] value = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        int size =servers.size();
+        int serverSelected;
+        
+        for(int i=1; i<=10; i++)
+        {
+        	serverSelected = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i)), 
+        			servers.size());
+        	servers.get(serverSelected).put(i, value[i-1]);
+        	System.out.println("Put (" + i + "  => " + value[i-1] + ") => Server http://localhost:300" + serverSelected) ;
+        }	
+        
+        for(int i=1;i<=10;i++)
+		{
+			serverSelected = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i)), 
+					servers.size());
+			System.out.println("Get (" + i + ") =>" + servers.get(serverSelected).get(i) + ") => Server http://localhost:300" 
+					+ serverSelected );
+		
+		}
         System.out.println("Existing Cache Client...");
-    }
+    }   
 
 }
